c0720dc6d26cb4683d474ddccbfa481c9bdb78db
aaronhtan <aaronhtan@gmail.com>
2025-08-08 15:09:08 +0100

    Finished Setup, beginning actual implementation of game

---
+++ src/starpython/__main__.py
@@ -0,0 +1,25 @@
+'''
+Main function to run starpython. Run it via python -m starpython
+'''
+import pygame
+
+
+def game_loop():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+
+        screen.fill((0, 0, 0))
+        clock.tick(60)  # 60 FPS
+
+
+def main():
+    game_loop()
+    return
+
+
+if __name__ == "__main__":
+    main()
